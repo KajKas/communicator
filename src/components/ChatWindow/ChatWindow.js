103b4399a195d1dd7357bd3ba4ebebf398f71457
kajetan.kasperuk <kajetan.kasperuk@neoteric.eu>
2019-06-27 14:21:47 +0200

    display messages

--- src/components/ChatWindow/ChatWindow.js
+++ src/components/ChatWindow/ChatWindow.js
@@ -1,14 +1,18 @@
 import React from "react"
+import _ from 'lodash'
 import store from '../../redux/store'
 import Header from "../Header/Header";
+import Chats from "../Chats/Chats";
 
 const ChatWindow = ({ activeUserId }) => {
   const state = store.getState()
   const activeUser = state.contacts[activeUserId]
+  const activeMessages = state.messages[activeUserId]
 
   return (
     <div className="ChatWindow">
       <Header user={activeUser} />
+      <Chats messages={_.values(activeMessages)} />
     </div>
   )
 }
