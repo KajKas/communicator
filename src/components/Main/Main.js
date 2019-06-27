2a5be10f69d9b568d160dc98f3e7bedb770b7fff
kajetan.kasperuk <kajetan.kasperuk@neoteric.eu>
2019-06-27 13:11:45 +0200

    display different component depending on the active user

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,17 @@
 import React from "react"
 import "./Main.css"
+import Empty from '../Empty/Empty'
+import ChatWindow from '../ChatWindow/ChatWindow'
 
-const Main = () => {
-  return <main className="Main">Main Stuff</main>
+const Main = ({ user, activeUserId }) => {
+  const renderMainContent = () => {
+    return (
+      activeUserId
+        ? <ChatWindow activeUserId={activeUserId} />
+        : <Empty user={user} />
+    )
+  }
+  return <main className='Main'>{renderMainContent()}</main>
 }
 
 export default Main
--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ import './index.css';
 import store from './redux/store'
 
 const render = () => {
+  fancyLog();
   return ReactDOM.render(
     <App />,
     document.getElementById('root')
   );
+}
+
+function fancyLog() {
+  console.log("%c Rendered with 👉 👉 👇 ", "background: purple; color: #FFF");
+  console.log(store.getState());
 }
 
 render()
