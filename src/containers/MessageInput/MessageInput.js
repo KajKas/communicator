4b9e5a534f4ab51d62893c609c283c10a907bde9
kajetan.kasperuk <kajetan.kasperuk@neoteric.eu>
2019-06-27 15:19:15 +0200

    send messages and scroll to bottom

--- src/containers/MessageInput/MessageInput.js
+++ src/containers/MessageInput/MessageInput.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import './MessageInput.css'
 import store from '../../redux/store'
-import { setTypingValue } from "../../redux/actions"
+import { setTypingValue, sendMessage } from "../../redux/actions"
 
 const MessageInput = ({ value }) => {
+  const state = store.getState()
   const handleChange = e => {
     store.dispatch(setTypingValue(e.target.value))
   }
+  const handleSubmit = e => {
+    e.preventDefault()
+    const { typing, activeUserId } = state
+    store.dispatch(sendMessage(typing, activeUserId))
+  }
 
   return (
-    <form className="Message">
+    <form className="Message" onSubmit={handleSubmit}>
       <input
         className="Message__input"
         onChange={handleChange}
--- src/redux/reducers/messages.js
+++ src/redux/reducers/messages.js
@@ -1,5 +1,25 @@
 import { getMessages } from "../../static-data"
+import { SEND_MESSAGE } from "../actions/constants/action-types"
+import _ from 'lodash'
 
 export default function messages(state = getMessages(20), action) {
-  return state
+  switch (action.type) {
+    case SEND_MESSAGE:
+      const { message, userId } = action.payload
+      const allUserMessages = state[userId]
+      const number = _.keys(allUserMessages).length
+      return {
+        ...state,
+        [userId]: {
+          ...allUserMessages,
+          [number]: {
+            number,
+            text: message,
+            is_user_msg: true
+          }
+        }
+      }
+    default:
+      return state
+  }
 }
